bd8b3d9b01206285d5bb2b52501a6e5539148f37
Yan Fonseca <yangoular@gmail.com>
2023-02-25 00:11:26 -0300

    Criação de métodos estáticos circunferencia, volume e PI.

--- Projeto08/Program.cs
+++ Projeto08/Program.cs
@@ -11,14 +11,20 @@ namespace Projeto08
             Console.WriteLine("Insira o valor do raio; ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
             double circ = Circunferencia(raio);
+            double vol = Volume(raio);
 
             Console.WriteLine("Circunferência; " + circ.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume; " + vol.ToString("F2",CultureInfo.InvariantCulture));
 
             Console.ReadLine();
         }
         static double Circunferencia(double r)
         {
             return 2.0 * Pi * r;
+        }
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r,3.0);
         }
     }
 }
