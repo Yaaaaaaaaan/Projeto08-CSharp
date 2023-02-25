8b01d260ff587beb0a9f07ab01b905c356bfce84
Yan Fonseca <yangoular@gmail.com>
2023-02-25 03:09:03 -0300

    Inserção de prefixo estático para Pi, Cicrunferência, Volume. Além da remoção do método cal (calculadora)

--- Projeto08/Program.cs
+++ Projeto08/Program.cs
@@ -7,15 +7,14 @@ namespace Projeto08
     {
         static void Main(string[] args)
         {
-            Calculadora cal = new Calculadora();
             Console.WriteLine("Insira o valor do raio; ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
-            double circ = cal.Circunferencia(raio);
-            double vol = cal.Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência; " + circ.ToString("F2",CultureInfo.InvariantCulture));
             Console.WriteLine("Volume; " + vol.ToString("F2",CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI; " + cal.Pi.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI; " + Calculadora.Pi.ToString("F2",CultureInfo.InvariantCulture));
             Console.ReadLine();
         }
     }
