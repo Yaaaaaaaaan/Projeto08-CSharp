ec18ad916415a94c608a8178838c8db8787778a2
Yan Fonseca <yangoular@gmail.com>
2023-02-24 23:59:22 -0300

    Remoção de bibliotecas não utilizadas.

--- Projeto08/Program.cs
+++ Projeto08/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Globalization;
 
 namespace Projeto08
@@ -12,11 +8,13 @@ namespace Projeto08
         static double Pi = 3.14;
         static void Main(string[] args)
         {
-            Console.WriteLine(  "Insira o valor do raio; ");
+            Console.WriteLine("Insira o valor do raio; ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
             double circ = Circunferencia(raio);
 
             Console.WriteLine("Circunferência; " + circ.ToString("F2",CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
         }
         static double Circunferencia(double r)
         {
