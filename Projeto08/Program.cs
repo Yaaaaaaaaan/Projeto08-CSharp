31d39d4a23233dc6d00682eac3c6b41e7939811c
Yan Fonseca <yangoular@gmail.com>
2023-02-25 02:59:51 -0300

    Criação da Classe Calculadora, e tranferência de métodos já feitos.

--- Projeto08/Program.cs
+++ Projeto08/Program.cs
@@ -5,26 +5,18 @@ namespace Projeto08
 {
     internal class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
+            Calculadora cal = new Calculadora();
             Console.WriteLine("Insira o valor do raio; ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
-            double circ = Circunferencia(raio);
-            double vol = Volume(raio);
+            double circ = cal.Circunferencia(raio);
+            double vol = cal.Volume(raio);
 
             Console.WriteLine("Circunferência; " + circ.ToString("F2",CultureInfo.InvariantCulture));
             Console.WriteLine("Volume; " + vol.ToString("F2",CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI; " + Pi.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI; " + cal.Pi.ToString("F2",CultureInfo.InvariantCulture));
             Console.ReadLine();
-        }
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(r,3.0);
         }
     }
 }
